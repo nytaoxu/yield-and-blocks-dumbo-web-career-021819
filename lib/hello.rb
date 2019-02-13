0a4828d309b7e2c8151ebe53d5686370c18b71c4
Tao Xu <nytaoxu@gmail.com>
2019-02-13 21:06:09 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,16 @@
 def hello_t(arg)
-  i = 0
-  
-  while i < arg.length
-    yield arg[i]
-    i += 1
+  if block_given?
+    i = 0
+    
+    while i < arg.length
+      yield arg[i]
+      i += 1
+    end
+    
+    arg
+  else
+    puts "no block given."
   end
-  
-  arg
 end
 
 # call your method here!
